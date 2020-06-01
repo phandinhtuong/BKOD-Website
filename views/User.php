50b239519bb28cf230676a6549fea346c88b2fcf
nam <nam98nd16@gmail.com>
2020-06-01 15:37:29 +0700

    add update user's information functionality

--- views/User.php
+++ views/User.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION["u"])) {
   session_destroy();
 }
 
-include ("../controllers/UserInfoController.php");
+include("../controllers/UserInfoController.php");
 $user = getUsersInfo();
 ?>
 
@@ -28,33 +28,42 @@ $user = getUsersInfo();
 
   <?php include 'NavBar.php'; ?>
 
-  <div class="container">
+  <?php if (isset($_SESSION["updateUserInfoSuccess"])) {
+    echo "<h2 align='center'>Updated successfully!</h2>";
+    unset($_SESSION["updateUserInfoSuccess"]);
+  } ?>
 
-    <label for="fullName"><b>Full Name</b></label>
-    <input type="text" placeholder="Enter Full Name" name="fullName" id="fullName" value="<?php echo $user[1] ?>" required>
+  <form action="../controllers/UserInfoController.php" method="POST">
+    <div class="container">
 
-    <label for="school"><b>School</b></label>
-    <input type="text" placeholder="Enter School" name="school" id="school" value="<?php echo $user[6] ?>">
+      <label for="fullName"><b>Full Name</b></label>
+      <input type="text" placeholder="Enter Full Name" name="fullName" id="fullName" value="<?php echo $user[1] ?>" required>
 
-    <label for="class"><b>Class</b></label>
-    <input type="text" placeholder="Enter Class" name="class" id="class" value="<?php echo $user[7] ?>">
+      <label for="school"><b>School</b></label>
+      <input type="text" placeholder="Enter School" name="school" id="school" value="<?php echo $user[6] ?>">
 
-    <label for="phone"><b>Phone Number</b></label>
-    <input type="text" placeholder="Enter Phone Number" name="phone" id="phone" value="<?php echo $user[8] ?>">
+      <label for="class"><b>Class</b></label>
+      <input type="text" placeholder="Enter Class" name="class" id="class" value="<?php echo $user[7] ?>">
 
-    <label for="birthday"><b>Birthday</b></label><br>
-    <input type="date" id="birthday" name="birthday" value="<?php echo $user[4] ?>"><br /> <br>
+      <label for="phone"><b>Phone Number</b></label>
+      <input type="text" placeholder="Enter Phone Number" name="phone" id="phone" value="<?php echo $user[8] ?>">
 
-    <label for="gender"><b>Gender</b></label><br />
-    <input type="radio" id="male" name="gender" value="1" <?php if ($user[5] == 1) echo "checked" ?>>
-    <label for="male">Male</label>
-    <input type="radio" id="female" name="gender" value="2" <?php if ($user[5] == 2) echo "checked" ?>>
-    <label for="female">Female</label>
-    <input type="radio" id="other" name="gender" value="3" <?php if ($user[5] == 3) echo "checked" ?>>
-    <label for="other">Other</label>
-    <input type="radio" id="unrevealed" name="gender" value="0" <?php if ($user[5] == 0) echo "checked" ?>>
-    <label for="unrevealed">Unrevealed</label><br><br>
+      <label for="birthday"><b>Birthday</b></label><br>
+      <input type="date" id="birthday" name="birthday" value="<?php echo $user[4] ?>"><br /> <br>
 
-  </div>
+      <label for="gender"><b>Gender</b></label><br />
+      <input type="radio" id="male" name="gender" value="1" <?php if ($user[5] == 1) echo "checked" ?>>
+      <label for="male">Male</label>
+      <input type="radio" id="female" name="gender" value="2" <?php if ($user[5] == 2) echo "checked" ?>>
+      <label for="female">Female</label>
+      <input type="radio" id="other" name="gender" value="3" <?php if ($user[5] == 3) echo "checked" ?>>
+      <label for="other">Other</label>
+      <input type="radio" id="unrevealed" name="gender" value="0" <?php if ($user[5] == 0) echo "checked" ?>>
+      <label for="unrevealed">Unrevealed</label><br><br>
+
+      <div class="button-holder"><button type="submit" class="save-btn">Save</button></div>
+
+    </div>
+  </form>
 </body>
 <?php include 'static/footer.html'; ?>
