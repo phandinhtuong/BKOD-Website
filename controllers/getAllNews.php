a12c11e03280b4bb1507d897eab1fde4189c4354
nam <nam98nd16@gmail.com>
2020-06-06 19:40:07 +0700

    init news page

---
+++ controllers/getAllNews.php
@@ -0,0 +1,47 @@
+<?php
+require '../utils/db_connection.php';
+header('Content-Type: application/json');
+
+$getAllUsersQuery = $db->prepare("SELECT * FROM user");
+if (PEAR::isError($getAllUsersQuery)) {
+  echo "Bad query detected!";
+}
+
+$res = &$db->execute($getAllUsersQuery);
+
+if (PEAR::isError($res)) {
+  $err = $res->getDebugInfo();
+  echo json_encode("An unknown error occured!");
+} else {
+  $allUsers = array();
+  $count = 0;
+  while (($user = $res->fetchRow())) {
+    $count++;
+    $trueUser = array();
+    $trueUser["no"] = $count;
+    foreach ($user as $key=>$value) {
+      if ($key === 0)
+        $trueUser["userId"] = $value;
+      else if ($key === 1)
+      $trueUser["username"] = $value;
+      else if ($key === 3)
+      $trueUser["fullName"] = $value;
+      else if ($key === 4)
+      $trueUser["birthday"] = $value;
+      else if ($key === 5)
+      $trueUser["gender"] = $value;
+      else if ($key === 6)
+      $trueUser["school"] = $value;
+      else if ($key === 7)
+      $trueUser["class"] = $value;
+      else if ($key === 8)
+      $trueUser["phoneNumber"] = $value;
+      else if ($key === 9)
+      $trueUser["isCounselor"] = $value;
+      else if ($key === 10)
+      $trueUser["state"] = $value;
+    }
+    $allUsers[] = $trueUser;
+  }
+  echo json_encode($allUsers);
+}
