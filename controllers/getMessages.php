7349684eb92918f907bb512ba9b39529b044c20e
Pham Hong Quang 20163316 <quang.ph163316@sis.hust.edu.vn>
2020-06-09 18:05:42 +0700

    Refactor code MVC

--- controllers/getMessages.php
+++ controllers/getMessages.php
@@ -4,9 +4,12 @@ header('Content-Type: application/json');
 include ("../models/MessageModel.php");
 $userName = $_POST['userName'];
 include ("../models/UserModel.php");
-$userModel = new UserModel($userName);
+$userModel = new UserModel();
 $userId = $userModel->getUserId($userName);
-$messageModel = new MessageModel($userId);
+
+$messageModel = new MessageModel();
+$messageModel->setCurrentUserId($userId);
+
 echo $messageModel->getAllMessages();
 
 ?>
