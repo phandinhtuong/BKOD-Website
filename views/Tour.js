98e9dee5f7556be867619240d1d1b4bc6b33304e
Tuong Phan <tramphongsts@gmail.com>
2020-06-11 11:50:02 +0700

    display all tours successfully

---
+++ views/Tour.js
@@ -0,0 +1,111 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var xmlHttp;
+function displayAllTours() {
+
+    xmlHttp = GetXmlHttpObject();
+    if (xmlHttp == null)
+    {
+        alert("Browser does not support HTTP Request");
+        return;
+    }
+    $q = "getAllTours";
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4) {
+            //Response Text
+//                document.getElementById("main").innerHTML = xmlHttp.responseText;
+            //Response XML
+            var xmlDoc = xmlHttp.responseXML;
+
+//            document.getElementById("name").innerHTML =
+//                    xmlDoc.getElementsByTagName("tour").length;
+//            document.getElementById("main").innerHTML =
+//                    xmlDoc.getElementsByTagName("tourID")[0].childNodes[0].nodeValue;
+            var ul = document.createElement('ul');
+            ul.setAttribute('id', 'tours-list');
+//            ul.setAttribute('id','tours-list')
+            document.getElementById('main').appendChild(ul);
+            
+            //display all tours
+            for (i = 0; i < xmlDoc.getElementsByTagName("tour").length; i++) {
+//                var li = document.createElement("li");
+                var li = document.createElement("li");
+                li.setAttribute('class', 'tour');
+//            li.setAttribute("id","new");
+//            li.setAttribute('class', 'item');
+                ul.appendChild(li);
+//            li.innerHTML = "111";
+//                li.innerHTML = xmlDoc.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+                
+                //click on one tour to display 
+                li.onclick = displayOneTour;
+                
+                //change cursor when point to the tour
+//                li.style.cursor = "pointer";
+                //li.setAttribute('onclick',displayOneTour());
+                
+                //map image of tour
+                var p = document.createElement("img");
+                p.setAttribute('width', 70);
+                p.setAttribute('height', 70);
+//                p.setAttribute('src', 'http://htqt.hust.edu.vn/imgs/maphnen.jpg');
+                p.setAttribute('src', xmlDoc.getElementsByTagName("mapImageUrl")[i].childNodes[0].nodeValue);
+                
+//                p.onclick = displayOneTour();
+                li.appendChild(p);
+                
+                //name of tour
+                var t = document.createElement("span");
+                t.innerHTML = xmlDoc.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+                li.appendChild(t);
+//                li.addEventListener('click',displayOneTour());
+//                p.innerHTML = "e";
+//                var ctx = p.getContext("2d");
+
+                //test link of map image
+//                var link = document.createElement("li");
+//                link.setAttribute('class','tour');
+//                ul.appendChild(link);
+//                link.innerHTML = xmlDoc.getElementsByTagName("mapImageUrl")[i].childNodes[0].nodeValue;
+                
+
+            }
+
+//            var li = document.createElement("li");
+//            li.setAttribute('class','tour');
+////            li.setAttribute("id","new");
+////            li.setAttribute('class', 'item');
+//            ul.appendChild(li);
+//            li.innerHTML = "111";
+//            li = document.createElement("li");
+//            li.setAttribute('class','tour');
+////            li.setAttribute("id","new");
+////            li.setAttribute('class', 'item');
+//            ul.appendChild(li);
+//            li.innerHTML = "111";
+//            document.getElementById("name").innerHTML =
+//                    xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+        }
+    }
+    xmlHttp.open("GET", "../controllers/tourController.php?q=" + $q, true);
+    xmlHttp.send();
+}
+function displayOneTour(){
+    alert('TODO: Display one tour');
+}
+function GetXmlHttpObject()
+{
+    var objXMLHttp = null;
+    if (window.XMLHttpRequest)
+    {
+        objXMLHttp = new XMLHttpRequest();
+    } else if (window.ActiveXObject)
+    {
+        objXMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return objXMLHttp;
+}
--- views/Tour.php
+++ views/Tour.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<<<<<<< Updated upstream
 <script src="static/getHeader.js"></script>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -13,7 +14,23 @@ if (!isset($_SESSION["u"])) {
 }
 ?>
 
+=======
+<head>
+    <script src="static/getHeader.js"></script>
+    <script src="Tour.js"></script>
+    <link rel=stylesheet href="static/Tour.css" type="text/css">
+</head>
+>>>>>>> Stashed changes
 <body>
+    <?php
+    session_start();
+    if (!isset($_SESSION["u"])) {
+        header('Location: index.php');
+        session_destroy();
+    }
+    ?>
+
+
     <div id="header">
         <p>
         <h1>Tours list</h1>
@@ -22,6 +39,7 @@ if (!isset($_SESSION["u"])) {
 
 <script src="static/getNavBar.js"></script>
 <div id="main">
+<<<<<<< Updated upstream
     <script type="text/javascript">
 //           function showTours(){
         xmlhttp = new XMLHttpRequest();
@@ -43,6 +61,13 @@ if (!isset($_SESSION["u"])) {
 //           showTour();
     </script>
     
+=======
+    <form>
+        <script type="text/javascript">
+            displayAllTours();
+        </script>
+    </form>
+>>>>>>> Stashed changes
 
 </div>
 </body>
