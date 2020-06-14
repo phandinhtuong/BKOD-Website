7e789263f20fc6869663f6ddfa23c115050a9848
Tuong Phan <tramphongsts@gmail.com>
2020-06-15 00:35:53 +0700

    edit tour on going

--- controllers/TourController/tourController.php
+++ controllers/TourController/tourController.php
@@ -3,5 +3,8 @@ include '../../models/tourModel.php';
 $q = $_GET['q'];
 if (strcmp($q, "getAllTours") == 0){
     getAllTours();
+} else if (strcmp($q, "editTour")==0){
+    $tourID = $_GET['tourID'];
+    editTour($tourID);
 }
 ?>
